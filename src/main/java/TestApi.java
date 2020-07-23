f86be736a3a902dfe3450bb07521855238bdc335
KatasonovaMasa <katasonova_masa@mail.ru>
2020-07-23 19:43:20 +0300

    Первый api тест

---
+++ src/main/java/TestApi.java
@@ -0,0 +1,38 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class TestApi {
+    @Test()
+    public void testGet() {
+        Response response1 =
+                given()
+                        .log().all()
+                        .when()
+                        .get("https://reqres.in/api/unknown")
+                        .then()
+                        .log().all()
+                        .statusCode(200)
+                        .extract()
+                        .response();
+        response1.getBody().print();
+    }
+    @Test
+    public void updateExistingPet() {
+            given()
+                .body("{\n" +
+                        "    \"name\": \"morpheus\",\n" +
+                        "    \"job\": \"leader\"\n" +
+                        "}")
+                .log().everything()
+                .contentType(ContentType.JSON)
+                .baseUri("https://reqres.in")
+                .post("/api/users/")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body(matchesJsonSchemaInClasspath("JsonSchema.json"));
+   }
+}
